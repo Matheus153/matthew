47854886f2118a9736a8b0e23d6684a1ac740065
mathe <santiosmatheus@gmail.com>
2021-06-27 13:19:15 -0400

    master

--- pages/serie/[slug].js
+++ pages/serie/[slug].js
@@ -1,48 +1,47 @@
-import Layout from '../../components/Layout'
-import { getAllFullSeries } from '../../lib/dato-cms'
+import Layout from '../../components/Layout';
+import { getAllFullSeries } from '../../lib/dato-cms';
 import SerieView from '../../components/SerieView';
 
 function SeriePage({ serie }) {
-    return (
-        <Layout 
-            title={serie.name}
-            path={`/${serie.slug}`}
-            description={serie.description}>
-
-            <SerieView serie={serie}/>
-
-        </Layout>
-    )
+  return (
+    <Layout
+      title={serie.name}
+      path={`/${serie.slug}`}
+      description={serie.description}
+    >
+      <SerieView serie={serie} />
+    </Layout>
+  )
 }
 
 export const getStaticProps = async ({ params }) => {
-    const slug = params?.slug; 
-    const series = await getAllFullSeries();
-    const serie = series.find((s) => s.slug === slug) || null;
-  
-    if (!serie) {
-      return {
-        notFound: true,
-      };
-    }
-  
-    return {
-      props: {
-        serie,
-        allSeries: series,
-      },
-      revalidate: 60,
-    };
-  };
+  const slug = params?.slug; 
+  const series = await getAllFullSeries();
+  const serie = series.find((s) => s.slug === slug) || null;
 
-  export const getStaticPaths = async () => {
-    const series = await getAllFullSeries();
-    const slugs = series.map((s) => ({ params: { slug: s.slug } }));
-  
+  if (!serie) {
     return {
-      paths: slugs,
-      fallback: false,
+      notFound: true,
     };
-  };
+  }
 
-export default SeriePage();
+  return {
+    props: {
+      serie,
+      allSeries: series,
+    },
+    revalidate: 60,
+  };
+};
+
+export const getStaticPaths = async () => {
+  const series = await getAllFullSeries();
+  const slugs = series.map((s) => ({ params: { slug: s.slug } }));
+
+  return {
+    paths: slugs,
+    fallback: false,
+  };
+};
+
+export default SeriePage;
